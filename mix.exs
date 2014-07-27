be0a340b6ef369029bcd0549684a8f0180105c3d
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-07-27 14:44:11 -0500

    Update to latest exrm

--- mix.exs
+++ mix.exs
@@ -19,6 +19,6 @@ defmodule Test.Mixfile do
   # To specify particular versions, regardless of the tag, do:
   # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
-    [{:exrm, "~> 0.12.4"}]
+    [{:exrm, "~> 0.12.11"}]
   end
 end
