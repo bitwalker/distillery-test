496634d8e289d3d74e9190ab87126acf72c51e7c
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-06-22 11:16:16 -0500

    Update deps

--- mix.exs
+++ mix.exs
@@ -20,6 +20,6 @@ defmodule Test.Mixfile do
   # To specify particular versions, regardless of the tag, do:
   # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
-    [{:exrm, "~> 0.15.2"}]
+    [{:exrm, "~> 0.17.0"}, {:relx, github: "erlware/relx"}]
   end
 end
