60ded8e4b84e181482de07fcf700335893049be4
Onorio Catenacci <ocatenacci@riis.com>
2015-11-20 08:14:43 -0500

    Adding a help screen and adding the list command

--- exsrv.cmd
+++ exsrv.cmd
@@ -8,30 +8,61 @@ SET ERTS_BIN_PATH=C:/progra~1/erl7.1/erts-7.1/bin
 SET ERLSRV_CMD=%ERTS_BIN_PATH%/erlsrv
 SET APPNAME=%1
 
+IF [%1]==[] (
+CALL :HELP exsrv.cmd
+GOTO :EOF
+)
 IF /I %COMMAND%==INSTALL (CALL :INSTALL)
 IF /I %COMMAND%==START (CALL :START)
 IF /I %COMMAND%==STOP (CALL :STOP)
 IF /I %COMMAND%==REMOVE (CALL :REMOVE)
+IF /I %COMMAND%==LIST (CALL :LIST)
+GOTO :EOF
+
+:HELP
+SETLOCAL
+ECHO %~1 appname command
+ECHO Where command can be "INSTALL", "START", "STOP", "LIST" or "REMOVE"
+ECHO Note that installing the service doesn't automatically start it but
+ECHO removing a service will attempt to stop it before removing it.
+ECHO You do have to pass an appname for the list command although the command
+ECHO will list all erlsrv apps regardless.
+ENDLOCAL
 GOTO :EOF
 
 :INSTALL
+SETLOCAL
 CALL :REMOVE
 ECHO Installing %APPNAME% service
 %ERLSRV_CMD% add %APPNAME% -machine %ERTS_BIN_PATH%/start_erl.exe -c "%APPNAME% Elixir Service" -w %TEMP% -n %APPNAME%@localhost -d reuse
+ENDLOCAL
 GOTO :EOF
 
 :REMOVE
+SETLOCAL
 CALL :STOP
 ECHO Removing %APPNAME% service
 %ERLSRV_CMD% remove %APPNAME%
+ENDLOCAL
 GOTO :EOF
 
 :START
+SETLOCAL
 %ERLSRV_CMD% start %APPNAME%
+ENDLOCAL
 GOTO :EOF
 
 :STOP
+SETLOCAL
 ECHO Stopping %APPNAME% service
 %ERLSRV_CMD% stop %APPNAME%
+ENDLOCAL
+GOTO :EOF
+
+:LIST
+SETLOCAL
+ECHO Listing all erlsrv services
+%ERLSRV_CMD% list
+ENDLOCAL
 GOTO :EOF
 
