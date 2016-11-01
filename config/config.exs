bc5de664da76df29ffa849f3ca04b21dec85e05e
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-11-01 13:01:01 -0500

    Silence warnings for conform/distillery

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,19 @@
 use Mix.Config
 
+config :distillery,
+  no_warn_missing: [
+    :distillery,
+    :conform
+  ]
+
 config :test,
   env: :wat,
   "debug_level": {:on, [:passive]}
 
 config :sasl,
   errlog_type: :error
+
+config :logger,
+  level: :debug
 
 import_config "config.#{Mix.env}.exs"
